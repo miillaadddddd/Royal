791f07ae12ec6e09bf07f932ee8ee3c323e83fd7
miillaadddddd <milad7212@gmail.com>
2021-08-14 19:20:43 +0430

    Add backgroundColor

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,49 @@ import {useState,React} from 'react'
 
 function App() {
 const [idcloth, setIdcloth] = useState(0)
-  const wichPic=()=>{
-    console.log(`hhhhhhhhhhhhhhh`, "hhhhhhhhhhhhhhh")
+  const wichPic=(id)=>{
+    console.log('hi :>> ', id);
+    setIdcloth(id)
   }
   return (
    <>
    <div className="main">
      <div className="left"> 
      <div className="left_child">
-       <button onclick={()=>wichPic}>
-         {/* <img src="assast/images/1.jpg" height="300" alt="one"
+       
+         <img onClick={()=>wichPic(1)} src="assast/images/1.jpg" height="300" alt="one"
          
-         /> */}hhhhhhhhhhhhhh
-       </button>
-       <img src="assast/images/2.jpg" height="300" alt="one" />
+         />
+     
+       <img onClick={()=>wichPic(2)} src="assast/images/2.jpg" height="300" alt="one" />
      </div>
      <div className="left_child">
-     <img src="assast/images/3.jpg" height="300" alt="one" />
-       <img src="assast/images/4.jpg" height="300" width="170" alt="one"/>
+     <img onClick={()=>wichPic(3)} src="assast/images/3.jpg" height="300" alt="one" />
+       <img onClick={()=>wichPic(4)} src="assast/images/4.jpg" height="300" width="170" alt="one"/>
      </div>
       </div>
      <div className="right">
-       {idcloth==0 && <video width="400" height="600"
+      {idcloth==0 &&  <video width="400" height="600"
        autoPlay={true}
         loop muted >
        <source src="assast/videos/0.mp4" type="video/mp4"/>
        </video>}
-       {idcloth==0 && <video width="400" height="600"
+       {idcloth==1 && <video width="400" height="600"
        autoPlay={true}
         loop muted >
        <source src="assast/videos/1.mp4" type="video/mp4"/>
        </video>}
-       {idcloth==0 && <video width="400" height="600"
+       {idcloth==2 && <video width="400" height="600"
        autoPlay={true}
         loop muted >
        <source src="assast/videos/2.mp4" type="video/mp4"/>
        </video>}
-       {idcloth==0 && <video width="400" height="600"
+       {idcloth==3 && <video width="400" height="600"
        autoPlay={true}
         loop muted >
        <source src="assast/videos/3.mp4" type="video/mp4"/>
        </video>}
-       {idcloth==0 && <video width="400" height="600"
+       {idcloth==4 && <video width="400" height="600"
        autoPlay={true}
         loop muted >
        <source src="assast/videos/4.mp4" type="video/mp4"/>
